3d6720b23c0ebc3f1b03dd9b485dfcabbe5f1294
James Robbins <jameslrobbins86@gmail.com>
2019-08-16 14:57:41 -0500

    Final Upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,105 +1,77 @@
-var gem1;
-var gem2;
-var gem3;
-var gem4;
+$( document ).ready(function() {
 
-var total;
+    // Variables--------------------------------------------------
+    var gem1 = Math.floor(Math.random() * 12) + 1;
+    var gem2 = Math.floor(Math.random() * 12) + 1;
+    var gem3 = Math.floor(Math.random() * 12) + 1;
+    var gem4 = Math.floor(Math.random() * 12) + 1;
 
-var wins;
-var losses;
+    var total = Math.floor(Math.random() * ((120-19)+1) + 19);
+    var userScore = 0;
+    var wins = 0;
+    var losses = 0;
+
+    // Gem buttons--------------------------------------------------
+
+    $("#gem1").on("click", function() {
+        userScore += gem1;
+        winlose();
+        $("#score").text(userScore);
+    });
+   $("#gem2").on("click", function() {
+        userScore += gem2;
+        winlose();
+        $("#score").text(userScore);
+    });
+    $("#gem3").on("click", function() {
+        userScore += gem3;
+        winlose();
+        $("#score").text(userScore);
+    });
+    $("#gem4").on("click", function() {
+        userScore += gem4;
+        winlose();
+        $("#score").text(userScore);
+    });
+
+    $("#win-num").text(total);
+    $("#score").text(userScore);
 
 
-function gameStart() {
-
-    var score = 0;
-
+    
+    
+    // Game reset--------------------------------------------------
+   function reset() {
     gem1 = Math.floor(Math.random() * 12) + 1;
     gem2 = Math.floor(Math.random() * 12) + 1;
     gem3 = Math.floor(Math.random() * 12) + 1;
     gem4 = Math.floor(Math.random() * 12) + 1;
-
     total = Math.floor(Math.random() * ((120-19)+1) + 19);
+    userScore = 0;
 
     $("#win-num").text(total);
+
+   };
+
+   //Win/lose function--------------------------------------------------
+
+   function winlose() {
+       if (userScore === total) {
+           alert("You win!");
+           wins++
+           reset();
+       } else if (userScore > total) {
+           alert("You lose!");
+           losses++
+           reset();
+       }
+
+    $("#wins").text("Wins: " + wins);
+    $("#losses").text("Losses: " + losses);
     
-    
-    
+   }  
 
-    $("#gem1").on("click", function() {
-        score = score + gem1;
-        console.log(score);
-        $("#score").text(score);
-        if (score === total) {
-            alert("You win!");
-            wins++
-            gameStart()
-        } else if (score > total) {
-            alert("You lose");
-            lose++
-            gameStart()
-        }
-       
-    })
-    $("#gem2").on("click", function() {
-        score = score + gem2;
-        console.log(score);
-        $("#score").text(score);
-        if (score === total) {
-            alert("You win!");
-            wins++
-            gameStart()
-        } else if (score > total) {
-            alert("You lose");
-            lose++
-            gameStart()
-        }
-    })
-
-    $("#gem3").on("click", function() {
-        score = score + gem3;
-        console.log(score);
-        $("#score").text(score);
-        if (score === total) {
-            alert("You win!");
-            wins++
-            gameStart()
-        } else if (score > total) {
-            alert("You lose");
-            lose++
-            gameStart()
-        }
-    })
-
-    $("#gem4").on("click", function() {
-        score = score + gem4;
-        console.log(score);
-        $("#score").text(score);
-        if (score === total) {
-            alert("You win!");
-            wins++
-            gameStart()
-        } else if (score > total) {
-            alert("You lose");
-            lose++
-            gameStart()
-        }
-        
-    })
-
-   
-
-    
-    
-    
-    
-}
-
-
-
-
-
-
-gameStart();
+})
 
 
 
